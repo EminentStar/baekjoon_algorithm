9ba8188599c183191edfb78f14a464c25ec2f19f
EminentStar <EminentStar@users.noreply.github.com>
2016-04-16 16:34:59 +0900

    Update 7569_3x3tomato_modifed1.cpp

--- 7569_3x3tomato_modifed1.cpp
+++ 7569_3x3tomato_modifed1.cpp
@@ -17,7 +17,7 @@
         혹시 break를 했었더라면
         결과 출력시 isWholeRiped ==false 인 분기부터 먼저 타고 들어가도록 했어야했다.
     Point-1: 아래의 코드 참조바람
-    ㅇ
+    Point-2: 아래의 코드를 참조바람
  */
 
 #include <iostream>
@@ -54,7 +54,20 @@ void bfs(){
                 continue;
             if(map[nextR][nextC][nextH] == -1) // empty position
                 continue;
-            if(step[nextR][nextC][nextH] == 0 ||(step[nextR][nextC][nextR] != 0 && (step[nextR][nextC][nextH] > step[curR][curC][curH] + 1))){ //unriped OR next riped days are longer than current riped day + 1
+            //if(step[nextR][nextC][nextH] == 0 ||(step[nextR][nextC][nextR] != 0 && (step[nextR][nextC][nextH] > step[curR][curC][curH] + 1))){ //unriped OR next riped days are longer than current riped day + 1
+                /*
+                    Point-2:
+                    (step[nextR][nextC][nextR] != 0 && (step[nextR][nextC][nextH] > step[curR][curC][curH] + 1)
+                    이조건의 의미는 내가 next로 탐색할 애가 쓰였는데 그게 최단이 아닐때인데
+                    애초에 처음에 익은 토마토를 큐에 다넣고 돌리면 그다음 큐에 들어오는 거는 하루 지난 토마토들만
+                    들어오게되지.. 그렇기때문에 지금 내가 방문할 수 있는게 최소한의 익는 일수라는 의미지..
+                    저조건은 안써줘도 된다.. 근데 이전에 내가 했던방법처럼 처음에 익은 토마토를 안넣고 시작하면,
+                    next > cur + 1 을 꼭 해줘야한다.. 
+                    그래서 큐 같은 경우는 처음 시작 조건이 될 수 있는거는 다 같이 넣고 시작하면 편하다..
+                    그러면 방문자체가 최소 거리로 방문하는게 되니깐,,
+                    ->그래서 방문한 것중에 최대 ㄱ밧을 구하면 그게 최대 일수가 되네..
+                */
+                if(step[nextR][nextC][nextH] == 0){ 
                 step[nextR][nextC][nextH] = step[curR][curC][curH] + 1;
                 /*
                     Point-1: 아래의 코드를 집어넣으면, 
